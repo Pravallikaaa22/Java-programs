3798cc82267faa37019354bd0aae86e95212e632
pravallika <pravallika@localhost>
2026-09-16 10:52:47 +0530

    practice

---
+++ myfirstjavaproject/src/com/javaintroduction/Testdemo2.java
@@ -0,0 +1,19 @@
+package com.javaintroduction;
+
+public class Testdemo2 {
+	static String collegeName = "Vcube";
+
+
+	public static void main(String[] args) {
+		String collegeName = "VSS";
+		System.out.println(collegeName);
+		System.out.println(Testdemo2.collegeName);
+		
+		
+		
+
+		
+
+	}
+
+}
